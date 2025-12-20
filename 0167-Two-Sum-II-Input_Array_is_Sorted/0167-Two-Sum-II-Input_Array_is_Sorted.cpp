246d0ee980c7f78f0c45aed1a3e171d3e4a7965a
Divyam Agarwal <divyam7246@gmail.com>
2025-12-20 16:55:07 +0530

    First Commit

---
+++ 0167-Two-Sum-II-Input_Array_is_Sorted/0167-Two-Sum-II-Input_Array_is_Sorted.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int currSum = 0 ;
+        int start = 0 ;
+        int end = numbers.size()-1;
+        vector<int> ans ;
+        while(start<end){
+            currSum = numbers[start] + numbers[end] ;
+            if (currSum == target){
+                ans.push_back(start+1);
+                ans.push_back(end+1);
+                return ans ;
+            }
+            else if(currSum > target){
+                end-- ;
+            }
+            else{
+                start++ ;
+            }
+        }
+        return ans ;
+    }
+};
