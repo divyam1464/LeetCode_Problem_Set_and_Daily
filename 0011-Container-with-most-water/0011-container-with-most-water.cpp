14d278b703122d458d88c3a69f58c43ed174e47a
Divyam Agarwal <divyam7246@gmail.com>
2025-12-21 16:29:59 +0530

    11 question of leetcode

---
+++ 0011-Container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {  // O(N)
+        int maxWater = 0 ;
+        int l = 0 , r = height.size()-1;
+        while(l<r){
+            int w = r-l ;
+            int h = min(height[l],height[r]);
+            int currWater = w*h;
+            maxWater = max(maxWater,currWater);
+            height[l]<height[r] ? l++ : r-- ;
+        }
+        
+        return maxWater ;
+    }
+};
